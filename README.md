72044b030e0cbb7cdc9352a77acd87773e180a9f
eliasanta <98754905+eliasanta@users.noreply.github.com>
2022-11-22 12:32:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-HCO
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-pg7hng)
---
+++ src/Users.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+
+const UserList = () => {
+  const [user, setUser] = useState([]);
+  return <div>user list</div>;
+};
+export default UserList ;
